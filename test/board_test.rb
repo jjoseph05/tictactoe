29c89a942572f4aa68681e18b04c5fd871cb4ad8
Andrew Bloom <andrew@andrewkbloom.com>
2013-06-09 16:53:50 -0500

    added a Board#make_move test

--- test/board_test.rb
+++ test/board_test.rb
@@ -16,4 +16,27 @@ class BoardTest < Test::Unit::TestCase
 
     assert_equal expected, str
   end
+
+  def test_make_move
+    @board.make_move("X", 0, 0)
+    assert_equal [
+      ['X', '-', '-'],
+      ['-', '-', '-'],
+      ['-', '-', '-']
+    ], @board.grid
+
+    @board.make_move("O", 1, 1)
+    assert_equal [
+      ['X', '-', '-'],
+      ['-', 'O', '-'],
+      ['-', '-', '-']
+    ], @board.grid
+
+    begin
+      @board.make_move("X", 1, 1)
+      assert false, "no exception raised"
+    rescue => e
+      assert_equal 'Cell (1, 1) is already occupied', e.message
+    end
+  end
 end
